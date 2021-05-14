bfb40208317899130c232ca54313d2da13b01591
Qi <qchen125@gmail.com>
2021-05-14 17:30:53 -0400

    update to 1 columns  need to fix

--- coinscraper.py
+++ coinscraper.py
@@ -96,15 +96,16 @@ def start_chrome():
     url = "https://coinmarketcap.com/"
     browser.get(url) #navigate to the page
     
-
+    #maximize the window (not really necessary but just waited to)
     browser.maximize_window()
     
-    wait = WebDriverWait(browser, 10)
+    wait = WebDriverWait(browser, 2)
+    #Search for every 20th element in the Table row
     men_menu = wait.until(ec.visibility_of_element_located((By.XPATH, '//*[@id="__next"]/div/div[1]/div[2]/div/div/div[2]/table/tbody/tr[20]')))                                      
     ActionChains(browser).move_to_element(men_menu).perform()
     # wait for element to appear, then hover it
-    
 
+    #Repeat the process 4 more times.
     men_menu = wait.until(ec.visibility_of_element_located((By.XPATH, '//*[@id="__next"]/div/div[1]/div[2]/div/div/div[2]/table/tbody/tr[40]')))
     ActionChains(browser).move_to_element(men_menu).perform()
     
@@ -138,10 +139,22 @@ def scrape(r):
     for coin in range(1,len(coins)):
         temp = coins[coin].findAll(text=True)
         # print('THIS IS THE TEMP')
-        # print(temp)
+        print(temp)
         newdata = []
+        if len(temp)==16:
+            newdata.append(temp[1])
+            newdata.append(temp[3])
+            newdata.append(temp[4])
+            # d = str()
+            newdata.append(temp[6]+temp[8])
+            # d = str()
+            newdata.append(temp[9]+temp[11])
+            newdata.append(temp[12])
+            newdata.append(temp[13])
+            a = temp[15].split()
+            newdata.append(a[0])
         if len(temp)==11:
-            print(temp)
+            # print(temp)
             newdata.append(temp[1])
             newdata.append(temp[3])
             newdata.append(temp[4])
@@ -151,11 +164,12 @@ def scrape(r):
             newdata.append(temp[8])
             a=temp[10].split()
             newdata.append(a[0])
-            now = datetime.now()
-            dt_string = now.strftime("%Y-%m-%d %H:%M:%S")
-            newdata.append(dt_string)
 
-            alldata.append(newdata)
+
+        now = datetime.now()
+        dt_string = now.strftime("%Y-%m-%d %H:%M:%S")
+        newdata.append(dt_string)
+        alldata.append(newdata)
         # print()
         # print("ALL THE DATA")
         # print(alldata)
@@ -223,7 +237,7 @@ def connect_db(alldata):
             market.append(i[7])
             #append tuple to list
             marketdata.append(tuple(market))
-        
+
 
         #Insert Data into Market Data table
         market_insert_query = """INSERT INTO MARKETDATA (PULLTIME , Name , Price, PER_CHANGE_H , PER_CHANGE_D , 
@@ -235,7 +249,7 @@ def connect_db(alldata):
         #List all data from MARKET DATA TABLE
         c.execute("SELECT * FROM MARKETDATA")
         row = c.fetchall()
-        print(row)
+        # print(row)
         
     except Error as e:
         print(e)
@@ -254,8 +268,6 @@ def connect_db(alldata):
 page_data = start_chrome()
 coindatalist = scrape(page_data)
 
-print('THIS IS THE PAGE DATA SCRAPED')
-print(page_data)
 
 write_to_csv(coindatalist)
 connect_db(coindatalist)
